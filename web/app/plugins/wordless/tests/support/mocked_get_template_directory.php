37d7f26b8c88bf8904e60b5ef53fcc0119c570e7
Christian Foster <christian.foster@gmail.com>
2014-05-01 13:53:34 +1000

    Removed wordless

--- web/app/plugins/wordless/tests/support/mocked_get_template_directory.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-# mocking WP get_template_directory()
-function get_template_directory() {
-  return "/mocked/file/path/to/mocked_root/mocked_theme";
-}
--- web/app/plugins/wordless/wordless/theme_builder/vanilla_theme/config/initializers/wordless_preferences.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-/*
-* This is an auto-generated preferences file. If you want to configure Wordless preferences go to WordPress backend.
-*/
